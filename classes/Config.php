74253960a234cb11faa2cd8b6191966a9f1c2176
andrew <andrew@andrew-VirtualBox>
2014-05-06 14:16:16 +0100

    Merge origin/master
    Conflicts:
    	README.md

--- classes/Config.php
+++ classes/Config.php
@@ -1,9 +1,7 @@
 <?php
 
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * 
  */
 
 /**
@@ -12,5 +10,13 @@
  * @author andrew
  */
 class Config {
-    //put your code here
+    public static function get($path = null) {
+        if($path) {
+            $config = $GLOBALS['config'];
+            $path = explode('/', $path);
+            
+            print_r($path);
+            
+        }
+    }
 }
--- core/init.php
+++ core/init.php
@@ -13,6 +13,22 @@ $GLOBALS['config'] = array(
         'db' => 'utubeoop'
     ),
     'remember' => array(
-        
+        'cookie_name' => 'hash',
+        'cookie_expiry' => 604800        
+    ),
+    'session' => array(
+        'session_name' => 'user'
     )
-)
+);
+
+//require_once 'classes/Config.php';
+//require_once 'classes/Cookie.php';
+//require_once 'classes/DB.php';
+// etc etc
+// Much easier and quicker to use the following function:
+
+spl_autoload_register(function($class) {
+    require_once 'classes/' . $class . '.php';
+});
+
+require_once 'functions/sanitize.php';
